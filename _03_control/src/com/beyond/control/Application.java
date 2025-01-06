9b65e86603160e370f197d3b7a646ece79eea4ac
hjsong <dev_sklg0602@naver.com>
2025-01-06 14:07:23 +0900

    25.01.06 switch 완료

--- _03_control/src/com/beyond/control/Application.java
+++ _03_control/src/com/beyond/control/Application.java
@@ -7,13 +7,17 @@ public class Application {
     public static void main(String[] args) {
 
         A_if a_if = new A_if();
-        B_switch b_switch = new B_switch();
 
         // a_if.method1();
         // a_if.method2();
         // a_if.method3();
         // a_if.practice();
 
+        B_switch b_switch = new B_switch();
+
+        // b_switch.method1();
+        // b_switch.method2();
+        b_switch.practice();
 
     }
 
---
+++ _03_control/src/com/beyond/control/practice/B_switch.java
@@ -0,0 +1,141 @@
+package com.beyond.control.practice;
+
+import java.util.Scanner;
+
+public class B_switch {
+    /*
+    * switch 문
+    * [표현법]
+    * switch (값) { // 정수, 문자, 문자열
+    *   case 값:
+    *       .. 실행 코드 ..
+    *       break;
+    *   case 값:
+    *       .. 실행 코드 ..
+    *       break;
+    *   [default:
+    *       .. 실행 코드 ..
+    *   ] -- 생략 가능
+    * }
+    * */
+    
+    // 사용자로부터 과일 이름을 입력받고 입력한 과일의 가격을 출력하기
+    public void method1(){
+        int price = 0;
+        String fruit = null;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("과일 이름 > ");
+        fruit = scanner.nextLine();
+
+        switch (fruit){
+            case "포도":
+                price = 8000;
+                break;
+                
+            case "딸기":
+                price = 12000;
+                break;
+
+            case "사과":
+                price = 4000;
+                break;
+
+            default:
+                System.out.println("판매하지 않는 과일입니다.");
+
+                // return 아래 코드는 실행하지 않고 실행 흐름을 메소드를 호출한 쪽으로 돌려준다.
+                return;
+        }
+
+        System.out.printf("%s의 가격은 %d원 입니다.\n", fruit, price);
+    }
+
+    // 1 ~ 12월까지 입력받아서 해당하는 달의 마지막 날짜를 출력하기
+    // 31일까지 존재하는 월 : 1, 3, 5, 7, 8, 10, 12
+    // 30일까지 존재하는 월 : 4, 6, 9, 11
+    // 28일 또는 29일까지 존재하는 월 : 2
+    public void method2(){
+        int month = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("1월 ~ 12월까지 중 하나를 입력하세요 > ");
+        month = scanner.nextInt();
+
+        if(!((month >= 1) && (month <= 12))){
+            System.out.println("1 ~ 12월까지 입력하셔야 합니다.");
+
+            return;
+        } else {
+            switch (month) {
+                case 1, 3, 5, 7, 8, 10, 12:
+                    System.out.printf("%d월은 31일까지 입니다.\n", month);
+                    break;
+                case 4, 6, 9, 11:
+                    System.out.printf("%d월은 30일까지 입니다.\n", month);
+                    break;
+                case 2:
+                    System.out.printf("%d월은 28일 또는 29일까지 입니다.\n", month);
+                    break;
+            }
+        }
+    }
+
+    /*
+     * 실습 문제
+     *
+     * 두 정수를 입력받고 연산자 기호(+, -, *, /)를 입력받아서 계산을 출력하라
+     * 단, 연산자 기호를 잘못 입력하는 경우 "연산자를 잘못 입력하셨습니다." 출력
+     *
+     * 예시)
+     *   첫 번째 수 > 3
+     *   두 번째 수 > 4
+     *   연산자 입력(+, -, *, /) > +
+     *
+     *   3 + 4 = 7
+     *
+     *   첫 번째 수 > 3
+     *   두 번째 수 > 4
+     *   연산자 입력(+, -, *, /) > a
+     *
+     *   연산자를 잘못 입력하셨습니다.
+     */
+    public void practice() {
+        int num1 = 0;
+        int num2 = 0;
+        int result = 0;
+        char ch = '\u0000';
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("첫 번째 수 > ");
+        num1 = scanner.nextInt();
+        
+        System.out.print("두 번째 수 > ");
+        num2 = scanner.nextInt();
+
+        System.out.print("연산자 입력(+, -, *, /) > ");
+        ch = scanner.next().charAt(0);
+
+        switch (ch){
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("연산자를 잘못 입력하셨습니다.");
+
+                return;
+        }
+
+        System.out.printf("%d %c %d = %d\n", num1, ch, num2, result);
+    }
+
+}
