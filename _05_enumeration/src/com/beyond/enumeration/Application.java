fa4c4a275956562c575040a0b38e7ec8d521fc52
hjsong <dev_sklg0602@naver.com>
2025-01-07 17:39:57 +0900

    25.01.07 열거 타입 진행중

---
+++ _05_enumeration/src/com/beyond/enumeration/Application.java
@@ -0,0 +1,31 @@
+package com.beyond.enumeration;
+
+import com.beyond.enumeration.practice.Week;
+
+import java.util.Arrays;
+
+public class Application {
+    public static void main(String[] args) {
+        // Week today = null;
+        // today = Week.TUESDAY;
+        Week today = Week.FRIDAY;
+
+        // 열거 상수에서 제공하는 메소드
+        System.out.println(today.name());
+        System.out.println(today.ordinal());
+        System.out.println(today.equals(Week.MONDAY));
+        System.out.println(today.compareTo(Week.MONDAY));
+        
+        // 열거 타입에서 기본적으로 제공하는 정적 메소드
+
+        // Week saturday = Week.valueOf("SATURDAY");
+        // System.out.println(saturday.name());
+        System.out.println(Week.valueOf("SATURDAY").name()); // 인텔리제이 shortcut [ctrl + alt + n] : 변수 인라인화
+
+        // Week[] values = Week.values();
+        // System.out.println(Arrays.toString(values));
+        System.out.println(Arrays.toString(Week.values())); // 인텔리제이 shortcut [ctrl + alt + n] : 변수 인라인화
+
+
+    }
+}
---
+++ _05_enumeration/src/com/beyond/enumeration/practice/Week.java
@@ -0,0 +1,11 @@
+package com.beyond.enumeration.practice;
+
+public enum Week {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
